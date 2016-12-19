e66299b94625327122e5b3938b88936f44ff0385
GuanAllen <tsguan11@gmail.com>
2016-12-19 19:26:16 +0800

    commit

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,19 +1,19 @@
 class Cart
   attr_reader :items
 
-  def initialize
-    @items = []
+  def initialize(items = [])
+    @items = items
   end
 
   def add_item(product_id)
     # 檢查是否有重複
-    item = items.find{ |i| i.product_id == product_id}
+    item = items.find { |i| i.product_id == product_id }
 
     if item
-      # 有的話，數量加1
+      # 有的話，數量加 1
       item.increment
     else
-      # 沒有的話，加入items
+      # 沒有的話，加入 items
       @items << CartItem.new(product_id)
     end
   end
@@ -23,7 +23,23 @@ class Cart
   end
 
   def total_price
-    total = 0
     items.reduce(0) { | sum, item| sum + item.total_price }
   end
+
+  def serialize
+    result = items.map { |item| {"product_id" => item.product_id,
+                                 "quantity" => item.quantity} }
+    { "items" => result }
+  end
+
+  def self.build_from_hash(hash)
+    all_items = []
+    if hash && hash["items"]
+      all_items = hash["items"].map do |item|
+        CartItem.new(item["product_id"], item["quantity"])
+      end
+    end
+    new all_items
+  end
 end
+
